dcd7cbb5887561a45995930febdfc896c6c246c1
AnkitBisen <bisenankit21@gmail.com>
2024-05-15 23:53:14 +0530

    Updated files

--- src/main/java/com/example/CrudUsingSpringboot/rest/StudentRestController.java
+++ src/main/java/com/example/CrudUsingSpringboot/rest/StudentRestController.java
@@ -58,4 +58,16 @@ public class StudentRestController {
         //return ResponseEntity
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
+    //add another handler ...to catch any execption
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        //create a studemtErrorResponse
+        StudentErrorResponse error=new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        //return ResponseEntity
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+
+    }
 }
